ab63e79eec81c5dbe16dbd524fcf163e56db76e9
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro-2.local>
2020-07-18 09:28:31 +0200

    add basic test for loader , add loder comp

---
+++ appointments/src/AppointmentFormLoader.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+export const AppointmentFormLoader = () => {
+  useEffect(() => {
+    const fetchAvailableTimeSlots = () => {
+      window.fetch('/availableTimeSlots', {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' },
+      });
+    };
+    fetchAvailableTimeSlots();
+  }, []);
+  return null;
+};
