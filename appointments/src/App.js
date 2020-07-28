645f55e490855f4f86ec5bc633affcf87b436584
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro-2.local>
2020-07-28 19:25:02 +0200

    app

---
+++ appointments/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppointmentsDayViewLoader } from './AppointmentsDayViewLoader';
+
+export const App = () => (
+  <React.Fragment>
+    <AppointmentsDayViewLoader />
+  </React.Fragment>
+);
---
+++ appointments/test/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  child,
+  createShallowRenderer,
+  type,
+} from './shallowHelpers';
+import { App } from '../src/App';
+import { AppointmentsDayViewLoader } from '../src/AppointmentsDayViewLoader';
+
+describe('App', () => {
+  let render, elementMatching;
+  beforeEach(() => {
+    ({ render, elementMatching } = createShallowRenderer());
+  });
+  it('initially shows the AppointmentDayViewLoader', () => {
+    render(<App />);
+    expect(
+      elementMatching(type(AppointmentsDayViewLoader))
+    ).toBeDefined();
+  });
+  it('has a button bar as the first child', () => {
+    render(<App />);
+    expect(child(0).type).toEqual('div');
+    expect(child(0).props.className).toEqual('button-bar');
+  });
+});
